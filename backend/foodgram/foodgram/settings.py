98cf03145b2ae335b5538a7a654181b87e44c672
Andrey Shadrin <shdrn@yandex.ru>
2022-06-11 14:51:19 +0300

    +++++++++

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = int(os.getenv('DJANGO_DEBUG', default=0))
 
-ALLOWED_HOSTS = ['51.250.36.210', 'localhost', 'bestrecipes.ddns.net']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['51.250.36.210', 'localhost', 'bestrecipes.ddns.net']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
