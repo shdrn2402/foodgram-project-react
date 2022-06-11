ca448cf7fe27825354e5ef7692da5913b6c24ab8
Andrey Shadrin <shdrn@yandex.ru>
2022-06-11 11:14:35 +0300

    Change secrets name

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -69,8 +69,8 @@ DATABASES = {
             default='django.db.backends.postgresql'
         ),
         'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT')
     }
