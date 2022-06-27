05622aab975ae3480d8e9ac79785ac0dc68cda6e
Andrey Shadrin <shdrn@yandex.ru>
2022-06-27 15:10:08 +0300

    modify serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,5 +1,5 @@
 from api import models
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from users import models as users_models
