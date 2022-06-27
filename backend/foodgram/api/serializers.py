90589dca5afd813211703e82835749a78d5213b9
Andrey Shadrin <shdrn@yandex.ru>
2022-06-27 12:10:30 +0300

    Modify api/serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -109,29 +109,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
-    # def update(self, instance, validated_data):
-    #     ingredients = self.initial_data.get('ingredients')
-    #     instance.image = validated_data.get('image', instance.image)
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.text = validated_data.get('text', instance.text)
-    #     instance.cooking_time = validated_data.get(
-    #         'cooking_time', instance.cooking_time
-    #     )
-    #     instance.tags.clear()
-    #     tags_data = self.initial_data.get('tags')
-    #     instance.tags.set(tags_data)
-    #     models.RecipeIngredient.objects.filter(recipe=instance).all().delete()
-    #     for ingredient in ingredients:
-    #         models.RecipeIngredient.objects.bulk_create([
-    #             models.RecipeIngredient(
-    #                 recipe=instance,
-    #                 ingredient=ingredient.get('id'),
-    #                 amount=ingredient.get('amount')
-    #             )
-    #         ])
-    #     instance.save()
-    #     return instance
-
 
 class FavoriteSerializers(serializers.ModelSerializer):
     id = serializers.IntegerField(source='recipe.id')
